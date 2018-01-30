fac3f165b0dddedf8afdfa16d816468e215133a0
Jake Wags <35744963+JakeWags@users.noreply.github.com>
2018-01-30 10:56:21 -0800

    Full rework

--- clicker.js
+++ clicker.js
@@ -1,30 +1,66 @@
-var totalClicks = parseInt(localStorage.getItem("totalClicks")) || 0;
-//the OR zero just means that if localStorage.getItem("clicks") doesn't exist, use 0 instead
-var clickBonus = parseInt(localStorage.getItem("clickBonus")) || 1;
-var upgradeCost = localstorage.getItem("clickBonus") || 100;
-
-function addClick(){
-    totalClicks += clickBonus;
-    document.getElementById("clicks").innerHTML = totalClicks;
-    localStorage.setItem("totalClicks", totalClicks);
-}
-function increaseClickBonus(amount){
-    clickBonus += amount;
-    document.getElementById("clickBonus").innerHTML = clickBonus;
-    localStorage.setItem("clickBonus", clickBonus);
-}
-function buyUpgrade(){
-  if (totalClicks >= upgradeCost){
-    increaseClickBonus(1);
-    totalClicks -= upgradeCost;
-    upgradeCost *= 1.1;
-    document.getElementById("upgradeCost").innerHTML = upgradeCost;
-    localStorage.setItem("upgradeCost", upgradeCost);
+class Clicker{
+  constructor(){
+    this._totalClicks = parseInt(localStorage.getItem("totalClicks")) || 0;
+    this._clickBonus = parseInt(localStorage.getItem("clickBonus")) || 1;
+    this._upgradeCost = parseInt(localStorage.getItem("upgradeCost")) || 100;
   }
+  get totalClicks(){
+    return this._totalClicks;
+  }
+  get clickBonus(){
+    return this._clickBonus;
+  }
+  get upgradeCost(){
+    return this._upgradeCost;
+  }
+  resetClick(){
+    if(confirm("Are you sure?")){
+      localStorage.clear();
+      window.location.reload(false);
+    }
+  }
+  click(){
+    this._totalClicks += this._clickBonus;
+    this.updateDisplay();
+    this.updateStorage();
+  }
+  buyUpgrade(amount, costIncrement){
+    if(this._totalClicks >= this._upgradeCost){
+      this.increaseClickBonus(amount);
+      this._totalClicks -= this._upgradeCost;
+      this.increaseUpgradeCost(costIncrement);
+    }
+  }
+  increaseClickBonus(increment){
+    this._clickBonus += increment;
+    this.updateDisplay();
+    this.updateStorage();
+  }
+  increaseUpgradeCost(costIncrement){
+    this._upgradeCost = Math.floor(this._upgradeCost * costIncrement);
+    this.updateDisplay();
+    this.updateStorage();
+  }
+  updateDisplay(){
+    document.getElementById("clicks").innerHTML = this._totalClicks;
+    document.getElementById("clickBonus").innerHTML = this._clickBonus;
+    document.getElementById("upgradeCost").innerHTML = this._upgradeCost;
+  }
+  updateStorage(){
+    localStorage.setItem("totalClicks", this._totalClicks);
+    localStorage.setItem("clickBonus", this._clickBonus);
+    localStorage.setItem("upgradeCost", this._upgradeCost);
+  }
+}
+let click1 = new Clicker();
+function addClick(){
+  click1.click();
 }
 function reset(){
-  if(confirm("Are you sure?")){
-    localStorage.clear();
-    window.location.reload(false);
-  }
+  click1.resetClick();
 }
+function purchaseUpgrade(){
+  click1.buyUpgrade(1, 1.55);
+} 
+  
+  
