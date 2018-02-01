82e0a550934a458f41271031822f6f7f8c1a8fe9
Jake Wags <jakewagoneredu@gmail.com>
2018-01-31 16:30:09 -0800

    Changed theme

--- clicker.js
+++ clicker.js
@@ -5,17 +5,36 @@ class Clicker{
     this._upgradeCost = parseInt(localStorage.getItem("upgradeCost")) || 100;
     this._clickerImageSelector = parseInt(localStorage.getItem("clickerImageSelector")) || 0;
     this._clickerImages = [
-     "https://coubsecure-s.akamaihd.net/get/b93/p/coub/simple/cw_timeline_pic/00bf8b41367/141c3f734f40c9d7d74c0/med_1471891539_image.jpg",
-     "https://i.pinimg.com/originals/fc/a9/5c/fca95ce8f05ff975bd01b3e6de1a2cfd.jpg",
-     "https://videogamesftvms2015.files.wordpress.com/2015/02/virt_3.jpeg",
-     "https://im-01.gifer.com/7dnI.gif",
-     "https://i.ytimg.com/vi/9F8bs_THKiY/maxresdefault.jpg",
-     "https://i.imgur.com/NeZ7R.jpg",
-     "https://vignette.wikia.nocookie.net/southpark/images/c/cb/WorldofWarcraft11.jpg/revision/latest?cb=20100303001128",
-     "https://images1.houstonpress.com/imager/u/original/6368441/stickoftruth1.jpg",
-     "https://vignette.wikia.nocookie.net/spsot/images/4/4a/Cartman_Jimmy_the_bard.jpg/revision/latest?cb=20140324003817",
-     "http://assets.vg247.com/current//2014/07/south_park_oculus.jpg",
-     "http://cdn.mos.cms.futurecdn.net/69f022b412401c0b5acd47395a60e331-480-80.jpg",
+     /* 1 */"http://4.images.southparkstudios.com/blogs/southparkstudios.com/files/2014/05/0611_mongolian.jpg?quality=0.8",
+     /* 2 */"https://i.pinimg.com/originals/fc/a9/5c/fca95ce8f05ff975bd01b3e6de1a2cfd.jpg",
+     /* 3 */"https://videogamesftvms2015.files.wordpress.com/2015/02/virt_3.jpeg",
+     /* 4 */"https://im-01.gifer.com/7dnI.gif",
+     /* 5 */"https://i.ytimg.com/vi/9F8bs_THKiY/maxresdefault.jpg",
+     /* 6 */"https://i.imgur.com/NeZ7R.jpg",
+     /* 7 */"https://vignette.wikia.nocookie.net/southpark/images/c/cb/WorldofWarcraft11.jpg/revision/latest?cb=20100303001128",
+     /* 8 */"https://images1.houstonpress.com/imager/u/original/6368441/stickoftruth1.jpg",
+     /* 9 */"https://vignette.wikia.nocookie.net/spsot/images/4/4a/Cartman_Jimmy_the_bard.jpg/revision/latest?cb=20140324003817",
+     /* 10 */"http://assets.vg247.com/current//2014/07/south_park_oculus.jpg",
+     /* 11 */"http://cdn.mos.cms.futurecdn.net/69f022b412401c0b5acd47395a60e331-480-80.jpg",
+     /* 12 */"https://pbs.twimg.com/media/BemgoZGCYAAp_QU.jpg:large",
+     /* 13 */"https://cdn.theatlantic.com/static/mt/assets/international/South%20Park%20banner.jpg",
+     /* 14 */"http://media.comicbook.com/2016/07/south-park-fractured-but-whole-header-191801.jpg",
+     /* 15 */"http://digitalspyuk.cdnds.net/16/32/480x270/gallery-1471005298-south-park-indiana.jpg",
+     /* 16 */"https://i.imgur.com/JQw5TgG.png",
+     /* 17 */"http://o.aolcdn.com/hss/storage/midas/b506419d949a9305ba9640f02b2764af/204073011/maxresdefault4rt5z6uihhh.jpg",
+     /* 18 */"http://1.images.southparkstudios.com/blogs/southparkstudios.com/files/2014/07/1006-half-bear2.jpg",
+     /* 19 */"http://fandomania.com/wp-content/uploads/2009/10/wrestling02-600x297.jpg",
+     /* 20 */"http://2.images.southparkstudios.com/images/shows/south-park/clip-thumbnails/season-13/1310/south-park-s13e10c02-the-fine-sport-of-wrastling-16x9.jpg?width=1024&height=576",
+     /* 21 */"https://vignette.wikia.nocookie.net/southpark/images/c/ce/209.jpg/revision/latest?cb=20160411024525",
+     /* 22 */"https://i.ytimg.com/vi/pspyxUuR6Es/maxresdefault.jpg",
+     /* 23 */"https://i.ytimg.com/vi/01tWIKOqQHg/hqdefault.jpg",
+     /* 24 */"https://vignette.wikia.nocookie.net/degrassi/images/2/2e/SOUTH-PARK-S15-RoyalPudding-Ike.jpg/revision/latest?cb=20140316024141",
+     /* 25 */"http://1.images.southparkstudios.com/images/shows/south-park/clip-thumbnails/season-12/1212/south-park-s12e12c06-dont-jump-ike-4x3.jpg?width=100%&height=100%&quality=0.8",
+     /* 26 */"https://vignette.wikia.nocookie.net/southpark/images/2/21/MissTeacherBangsaBoy08.jpg/revision/latest?cb=20100303051752",
+     /* 27 */"http://basementrejects.com/wp-content/uploads/2012/09/south-park-season-13-8-dead-celebrities-ike-billy-mays.jpg",
+     /* 28 */"http://2.images.southparkstudios.com/images/shows/south-park/clip-thumbnails/season-10/1010/south-park-s10e10c04-afternoon-delight-4x3.jpg?width=100%&height=100%&quality=0.8",
+     /* 29 */"http://3.images.southparkstudios.com/images/shows/south-park/clip-thumbnails/season-17/1705/south-park-s17e05c06-come-on-bro-kick-the-baby-16x9.jpg?quality=0.8",
+     /* 30 */"https://vignette.wikia.nocookie.net/southpark/images/4/47/TamingStrangePromo.jpg/revision/latest?cb=20131028161640",
    ]
   }
   get totalClicks(){
@@ -69,7 +88,7 @@ class Clicker{
   }
   changePicture(){
     this._clickerImageSelector++;
-    if (this._clickerImageSelector >= 11){
+    if (this._clickerImageSelector >= this._clickerImages.length){
       this._clickerImageSelector = 0;
     }
   this.update();
