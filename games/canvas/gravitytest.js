2d28a4ac4a27c6b10fcec66c802c48c2bd6f4592
Jake Wags <jakewagoneredu@gmail.com>
2018-03-31 17:31:44 -0700

    added zlatan

--- games/canvas/gravitytest.js
+++ games/canvas/gravitytest.js
@@ -12,6 +12,8 @@ var mouse = {
   y: undefined
 }
 
+var bottomLine = innerHeight/1.25;
+
   window.addEventListener('resize',
     function() {
       canvas.width = window.innerWidth;
@@ -61,16 +63,27 @@ var mouse = {
 
 class Circle {
     constructor() {
-      this.x = innerWidth/2;
-      this.y = innerHeight/2;
-      this.radius = 50;
-      this.dy = 5;
-      this.dx = 0;
-      this.ax = 0;
-      this.ay = 1;
-      this.bottomLine = innerHeight/1.25;
-      this.isOnGround = false;
+      this._x = innerWidth/2;
+      this._y = innerHeight/2;
+      this._radius = 50;
+      this._dy = 5;
+      this._dx = 0;
+      this._ax = 0;
+      this._ay = 1;
+      this._isOnGround = false;
 }
+
+  get x() {
+    return this._x;
+  }
+
+  get y() {
+    return this._y;
+  }
+
+  get radius() {
+    return this._radius;
+  }
 
   draw() {
     c.beginPath();
@@ -85,76 +98,76 @@ class Circle {
     c.fillText('or use the arrow keys!', innerWidth/2, innerHeight/4 + 35);
 
     c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.arc(this._x, this._y, this._radius, 0, Math.PI * 2, false);
     c.fillStyle = "#ffffff";
     c.fill();
 
     c.beginPath();
     c.fillStyle = "#182233";
-    c.fillRect(0, this.bottomLine, innerWidth, (innerHeight - (innerHeight - this.bottomLine)))
+    c.fillRect(0, bottomLine, innerWidth, (innerHeight - (innerHeight - bottomLine)))
 }
 
   update() {
-    this.y += this.dy;
-    this.x += this.dx;
+    this._y += this._dy;
+    this._x += this._dx;
 
-  if ((this.y + this.radius) > this.bottomLine) {
-    this.isOnGround = true;
-    this.dy = 0;
-    this.ay = 0;
-    this.y = this.bottomLine - this.radius;
+  if ((this._y + this._radius) > bottomLine) {
+    this._isOnGround = true;
+    this._dy = 0;
+    this._ay = 0;
+    this._y = bottomLine - this._radius;
     console.log('on ground');
   } else {
-      this.dy += this.ay;
+      this._dy += this._ay;
   }
 
-  if (this.dx < 20 && this.dx > -20 || keyup) {
-    this.dx += this.ax;
-  }
-
-
-  if (this.x > innerWidth) {
-    this.x = 1;
-  } else if (this.x < 1) {
-    this.x = innerWidth;
+  if (this._dx < 20 && this._dx > -20 || keyup) {
+    this._dx += this._ax;
   }
 
 
-  if (this.dx === 0) {
-    this.ax = 0;
+  if (this._x > innerWidth) {
+    this._x = 1;
+  } else if (this._x < 1) {
+    this._x = innerWidth;
+  }
+
+
+  if (this._dx === 0) {
+    this._ax = 0;
   }
     this.draw();
   }
 
   jump(e) {
-    if(this.isOnGround) {
-      this.dy = -20;
-      this.ay = 1;
-      this.isOnGround = false;
+    if(this._isOnGround) {
+      this._dy = -20;
+      this._ay = 1;
+      this._isOnGround = false;
       console.log("we jumped!");
     }
   }
 
   recenter() {
-    this.x = innerWidth/2;
-    this.bottomLine = innerHeight/1.25;
-    this.y = this.bottomLine - this.radius;
-    this.ay = 1;
-    this.dy = 5;
+    this._x = innerWidth/2;
+    bottomLine = innerHeight/1.25;
+    this._y = bottomLine - this._radius;
+    this._ay = 1;
+    this._dy = 5;
   }
 
   move(direction) {
     switch (direction) {
       case 'left':
-        this.ax = -0.5;
+        this._ax = -0.5;
       break;
 
       case 'right':
-        this.ax = 0.5;
+        this._ax = 0.5;
       break;
 
       case 'stop':
-        this.ax *= -0.5;
+        this._ax *= -0.5;
       break;
     }
 }
